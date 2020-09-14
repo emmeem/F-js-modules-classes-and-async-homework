8a176abde5d09ffa0ae0b197484c8dae9ac806cd
emmeem <1124782639@qq.com>
2020-09-14 19:28:27 +0800

    p2 and p3

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,1 +1,13 @@
-export const getPoetry
+export const getPoetry = async () => {
+    const URL = 'https://v1.jinrishici.com/all.json';
+
+    const res = await fetch(URL)
+    const resJson = await res.json();
+
+    const result = [];
+    result.push(resJson.origin)
+    result.push(resJson.author)
+    result.push(resJson.content)
+    return result;
+}
+
