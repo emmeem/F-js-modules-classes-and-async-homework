daccd0c352437a6af1a50702d6d59081ab3567e9
xijing.gui <xijing.gui@thoughtworks.com>
2020-09-24 16:43:05 +0800

    add feedback

--- src/practice3/index.js
+++ src/practice3/index.js
@@ -1,4 +1,5 @@
 export const getPoetry = () => {
+    // TODO feedback：fetch默认的method就是get，第二个参数可以不用传
     return fetch("https://v1.jinrishici.com/all.json", {
         method: 'GET',
     })
@@ -7,7 +8,8 @@ export const getPoetry = () => {
         return [result.origin, result.author, result.content];
     })
     .catch((err) =>{
+        // TODO feedback:放了个表达式在这里，可以简单得console.log就行
         err.massage;
-    }); 
+    });
 }
 
